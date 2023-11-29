46e31abe08f3536d9bf6dba77ef5d6539b5da190
LabInfo <LabInfo@TERREOA2M14>
2023-11-29 20:41:06 -0300

    tabelei criação de relatórios

---
+++ FoodControl_Soluction/Food_Control/Formulários/frmVisualizaListaComidas.Designer.cs
@@ -0,0 +1,60 @@
+﻿
+namespace Food_Control.Formulários
+{
+    partial class frmVisualizaListaComidas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Location = new System.Drawing.Point(372, 79);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(396, 246);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // frmVisualizaListaComidas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1209, 556);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "frmVisualizaListaComidas";
+            this.Text = "frmVisualizaListaComidas";
+            this.Load += new System.EventHandler(this.frmVisualizaListaComidas_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+    }
+}
